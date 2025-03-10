262a108bab476cd6fc777db27fa0b32b74e09ae0
theo <theosvensson5@gmail.com>
2025-03-10 15:21:15 +0100

    ww

--- RepoLinter/GitIgnore.cs
+++ RepoLinter/GitIgnore.cs
@@ -2,7 +2,6 @@ namespace RepoLinter;
 
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
--- RepoLinter/obj/Debug/net8.0/RepoLinter.AssemblyInfo.cs
+++ RepoLinter/obj/Debug/net8.0/RepoLinter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepoLinter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb3ba0dd55fbbb0d22a578edad670667d96c5e07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dc9458fc813ecc72d60a5c4e0c53bbbb58c43e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepoLinter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepoLinter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
